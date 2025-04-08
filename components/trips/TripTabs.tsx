087228de18ec42d245a26c5e2be85015043c6000
Alexandre Archambault <archambaultalexandre@outlook.com>
2025-04-08 17:27:31 -0400

    Fix style, show trips based on selected tab

--- components/trips/TripTabs.tsx
+++ components/trips/TripTabs.tsx
@@ -1,6 +1,7 @@
 import {Button, StyleSheet, View, Text, Pressable} from "react-native";
 import { useState } from "react";
 import {Colors} from "@/utils/Colors";
+import {router} from "expo-router";
 
 interface TripTabsProps {
     tab: number;
@@ -12,20 +13,18 @@ export default function TripTabs({tab, setTab}: TripTabsProps) {
         <View style={styles.container}>
             <View style={styles.buttonContainer}>
                 <Pressable onPress={() => {setTab(1)}}>
-                    <Text>Upcoming</Text>
+                    <Text style={StyleSheet.compose(styles.buttonText, (tab === 1 ? styles.selectedButtonText : ""))}>Upcoming</Text>
                 </Pressable>
             </View>
             <View style={styles.buttonContainer}>
-                <Button
-                    onPress={() => {setTab(2)}}
-                    title="Past"
-                />
+                <Pressable onPress={() => {setTab(2)}}>
+                    <Text style={StyleSheet.compose(styles.buttonText, (tab === 2 ? styles.selectedButtonText : ""))}>Past</Text>
+                </Pressable>
             </View>
             <View style={styles.buttonContainer}>
-                <Button
-                    onPress={() => {setTab(3)}}
-                    title="Requested"
-                />
+                <Pressable onPress={() => {setTab(3)}}>
+                    <Text style={StyleSheet.compose(styles.buttonText, (tab === 3 ? styles.selectedButtonText : ""))}>Requested</Text>
+                </Pressable>
             </View>
         </View>
     );
@@ -36,12 +35,21 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         justifyContent: 'space-between',
         marginBottom: 10,
-        paddingHorizontal: 20,
+        paddingHorizontal: 20
     },
     buttonContainer: {
+        flex: 1
+    },
+    buttonText: {
+        color: Colors.gray,
+        fontSize: 16,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        paddingVertical: 10,
+    },
+    selectedButtonText: {
         color: Colors.flygreenGreen,
         borderBottomColor: Colors.flygreenGreen,
         borderBottomWidth: 1,
-        flex: 1
     }
 });
--- utils/Colors.tsx
+++ utils/Colors.tsx
@@ -2,4 +2,5 @@ export let Colors = {
     flygreenGreen: '#205046',
     gold: '#ffbf00',
     white: '#FFFFFF',
+    gray: '#808080',
 }
