752a6cc71fe054e097d1af8bd597d1369aaa2ca7
Alexandre Archambault <archambaultalexandre@outlook.com>
2025-06-09 12:37:23 -0400

    Styling changes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,21 @@
 import {Stack} from "expo-router";
+import { StatusBar } from 'expo-status-bar';
 
 export default function RootLayout() {
+
     return (
-        <Stack>
-            <Stack.Screen
-                name="(tabs)"
-                options={{ headerShown: false }}
-            />
-            <Stack.Screen
-                name="trip"
-                options={{headerShown: false}}
-            />
-        </Stack>
+        <>
+            <StatusBar style="dark" />
+            <Stack>
+                <Stack.Screen
+                    name="(tabs)"
+                    options={{ headerShown: false }}
+                />
+                <Stack.Screen
+                    name="trip"
+                    options={{headerShown: false}}
+                />
+            </Stack>
+        </>
     )
 }
--- app/trip/_layout.tsx
+++ app/trip/_layout.tsx
@@ -2,27 +2,31 @@ import {router, Slot, Stack, useLocalSearchParams} from "expo-router";
 import {Colors} from "@/utils/Colors";
 import {Button, Pressable, Text} from "react-native";
 import Ionicons from "@expo/vector-icons/Ionicons";
+import {StatusBar} from "expo-status-bar";
 export default function TripPageLayout() {
     return (
-        <Stack
-            screenOptions={{
-                headerBackVisible: false,
-                animationTypeForReplace: "pop",
-                title: "",
-                headerTitleStyle: {
-                    color: Colors.white,
-                },
-                headerLeft: () => (
-                    <Pressable
-                        onPress={() => router.back()}
-                    >
-                        <Ionicons name="arrow-back" size={24} color={Colors.white} />
-                    </Pressable>
-                ),
-                headerStyle: {
-                    backgroundColor: Colors.flygreenGreen,
-                }
-            }}
-        />
+        <>
+            <StatusBar style="light" />
+            <Stack
+                screenOptions={{
+                    headerBackVisible: false,
+                    animationTypeForReplace: "pop",
+                    title: "",
+                    headerTitleStyle: {
+                        color: Colors.white,
+                    },
+                    headerLeft: () => (
+                        <Pressable
+                            onPress={() => router.back()}
+                        >
+                            <Ionicons name="arrow-back" size={24} color={Colors.white} />
+                        </Pressable>
+                    ),
+                    headerStyle: {
+                        backgroundColor: Colors.flygreenGreen,
+                    }
+                }}
+            />
+        </>
     )
 }
