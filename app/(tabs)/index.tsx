c169cb1ecedcc406ed319e4dd7916283107b1684
Alexandre Archambault <archambaultalexandre@outlook.com>
2025-06-18 17:00:29 -0400

    Some changes to homepage

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,12 +2,13 @@ import {StyleSheet, Text} from 'react-native';
 import {SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import ProfilePage from "@/components/pages/ProfilePage";
 import {Colors} from "@/utils/Colors";
+import Homepage from "@/components/pages/Homepage";
 
 export default function Home() {
     return (
         <SafeAreaProvider>
             <SafeAreaView style={styles.container}>
-                <ProfilePage />
+                <Homepage/>
             </SafeAreaView>
         </SafeAreaProvider>
     );
--- components/pages/Homepage.tsx
+++ components/pages/Homepage.tsx
@@ -1,7 +1,27 @@
-import {Text} from "react-native";
+import {View, Text, StyleSheet} from "react-native";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 export default function Homepage() {
     return (
-        // <View></View>
-        <Text>Welcome to the Flygreen App!</Text>
+        <View style={styles.container}>
+            <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', marginBottom: 15}}>
+                <Text style={styles.title}>Welcome back Alex!</Text>
+                <MaterialIcons name="notifications" size={24} color="black" />
+            </View>
+            <View>
+                <Text>Here you can find all the latest updates and features.</Text>
+                <Text>Explore our new collections and offers.</Text>
+                <Text>Don't forget to check your notifications for any important updates.</Text>
+            </View>
+        </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 20,
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold'
+    }
+});
