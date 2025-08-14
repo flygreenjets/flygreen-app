147afd50230f7931df6ecb8767accaf6ea7abd1a
Alexandre Archambault <archambaultalexandre@outlook.com>
2025-08-14 11:16:59 -0400

    Dashboard changes

--- components/pages/Homepage.tsx
+++ components/pages/Homepage.tsx
@@ -1,37 +1,71 @@
-import {View, Text, StyleSheet, Button, Pressable, FlatList, Dimensions, ImageBackground} from "react-native";
+import {
+    View,
+    Text,
+    StyleSheet,
+    Button,
+    Pressable,
+    FlatList,
+    Dimensions,
+    ImageBackground,
+    ScrollView
+} from "react-native";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 import {useAuth} from "@/providers/AuthProvider";
 import TripCard from "@/components/trips/TripCard";
 import {router} from "expo-router";
+import {Colors} from "@/utils/Colors";
 
 const {width} = Dimensions.get("window");
 
 export default function Homepage() {
-    const trips = [
-        {
-            id: 2,
-            name: "Trip to New York",
-            description: "Description 1",
-            departureDate: "Tue, June 10, 2025",
-            departureAirport: {
-                code: "CYUL",
-                name: "Montreal Airport, QC",
-            },
-            destinationAirport: {
-                code: "KTEB",
-                name: "Newark Liberty Intl. Airport, NJ",
-            },
-            aircraft: {
-                category: 'Super Midsize Jet',
-                model: 'Citation X',
-                registration: 'C-GREEN',
-            },
-            stage: "Closed Won",
-            pax: 2,
-            duration: "1h 39m",
-            fuelStops: 0,
+    const nextTrip = {
+        id: 2,
+        name: "Trip to New York",
+        description: "Description 1",
+        departureDate: "Tue, June 10, 2025",
+        departureAirport: {
+            code: "CYUL",
+            name: "Montreal Airport, QC",
         },
-    ];
+        destinationAirport: {
+            code: "KTEB",
+            name: "Newark Liberty Intl. Airport, NJ",
+        },
+        aircraft: {
+            category: 'Super Midsize Jet',
+            model: 'Citation X',
+            registration: 'C-GREEN',
+        },
+        stage: "Closed Won",
+        pax: 2,
+        duration: "1h 39m",
+        fuelStops: 0,
+    };
+
+    const workingTrip =  {
+        id: 1,
+        name: "Trip to Miami",
+        description: "Description 1",
+        departureDate: "Fri, Apr 4, 2025",
+        stage: "Requested",
+        departureAirport: {
+            code: "KTEB",
+            name: "Newark Liberty Intl. Airport, NJ",
+        },
+        destinationAirport: {
+            code: "KTMB",
+            name: "Miami Executive Airport, FL",
+        },
+        aircraft: {
+            category: 'Light Jet',
+            model: 'Phenom 300',
+            registration: 'N12345',
+        },
+        pax: 5,
+        duration: "2h 30m",
+        fuelStops: 2,
+    }
+
 
     const recentDocs = [
         {id: '1', name: 'Quote 1'},
@@ -39,41 +73,47 @@ export default function Homepage() {
     ]
 
     return (
-        <View style={styles.container}>
-            <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', marginBottom: 15}}>
-                <Text style={styles.title}>Welcome back Alex!</Text>
-                <MaterialIcons name="notifications" size={24} color="black" />
-            </View>
-            <ImageBackground
-                source={{uri: 'https://flygreen.s3.us-east-2.amazonaws.com/pdf-assets/rewards/gold.jpg'}}
-                style={styles.rewardsContainer}
-                imageStyle={{borderRadius: 10}}
-                resizeMode={'cover'}
-            >
-                <Text style={{color: 'white', fontWeight: "bold", fontSize: 18}}>{(122354).toLocaleString()} Points</Text>
-                {/*<View>*/}
-                {/*    <Text style={{color: 'white'}}>1325 redeemable points</Text>*/}
-                {/*    <Text style={{color: 'white'}}>Status: Gold</Text>*/}
-                {/*</View>*/}
-            </ImageBackground>
-            <View style={{marginTop: 15, gap: 10}}>
-                <Text style={[styles.title, {fontSize: 16}]}>Your Next Trip</Text>
-                {trips.map((trip) => (
-                    <Pressable key={trip.id} onPress={() => router.push(`/trip/${trip.id}`)}>
-                        <TripCard trip={trip} />
+        <ScrollView style={styles.container} contentContainerStyle={{paddingBottom: 30}}>
+            <View style={{paddingHorizontal: 20}}>
+                <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', marginBottom: 15}}>
+                    <Text style={styles.title}>Ero Copper Corp.</Text>
+                    <MaterialIcons name="notifications" size={24} color="black" />
+                </View>
+                <ImageBackground
+                    source={{uri: 'https://flygreen.s3.us-east-2.amazonaws.com/pdf-assets/rewards/gold.jpg'}}
+                    style={styles.rewardsContainer}
+                    imageStyle={{
+                        borderRadius: 10,
+                        height: 205
+                    }}
+                    resizeMode={'stretch'}
+                >
+                    <Text style={{color: 'white', fontWeight: "bold", fontSize: 18}}>{(122354).toLocaleString()} Points</Text>
+                </ImageBackground>
+                <View style={{marginTop: 15, gap: 10}}>
+                    <Text style={[styles.title, {fontSize: 16}]}>Your next trip</Text>
+                    <Pressable key={nextTrip.id} onPress={() => router.push(`/trip/${nextTrip.id}`)}>
+                        <TripCard trip={nextTrip} />
                     </Pressable>
-                ))}
+                </View>
+                <View style={{gap: 10}}>
+                    <Text style={[styles.title, {fontSize: 16}]}>We're working on this trip</Text>
+                    <Pressable key={workingTrip.id} onPress={() => router.push(`/trip/${workingTrip.id}`)}>
+                        <TripCard trip={workingTrip} />
+                    </Pressable>
+                </View>
             </View>
             <View>
-                <Text style={[styles.title, {fontSize: 16}]}>Recent Documents</Text>
+                <Text style={[styles.title, {fontSize: 16, paddingHorizontal: 20}]}>Recently viewd</Text>
                 <FlatList
+                    contentInset={{top: 0, left: 20, bottom: 0, right: 20}}
                     style={{marginTop: 5}}
                     data={[
                         ...recentDocs
                     ]}
                     renderItem={({item}) => (
                         <Pressable onPress={() => console.log('Open document')}>
-                            <View style={[styles.recentDocItem, {backgroundColor: 'lightgray', borderRadius: 5}]}>
+                            <View style={styles.recentDocItem}>
                                 <Text>{item.name}</Text>
                                 <Text>CYUL to KTEB</Text>
                                 <Text>${(35949).toLocaleString()}</Text>
@@ -86,13 +126,36 @@ export default function Homepage() {
                     horizontal={true}
                 />
             </View>
-        </View>
+            <View style={{marginTop: 15}}>
+                <Text style={[styles.title, {fontSize: 16, paddingHorizontal: 20}]}>Recently shared with you</Text>
+                <FlatList
+                    contentInset={{top: 0, left: 20, bottom: 0, right: 20}}
+                    style={{marginTop: 5}}
+                    data={[
+                        ...recentDocs
+                    ]}
+                    renderItem={({item}) => (
+                        <Pressable onPress={() => console.log('Open document')}>
+                            <View style={styles.recentDocItem}>
+                                <Text>{item.name}</Text>
+                                <Text>CYUL to KTEB</Text>
+                                <Text>${(35949).toLocaleString()}</Text>
+                            </View>
+                        </Pressable>
+                    )}
+                    keyExtractor={(item) => item.id}
+                    ListEmptyComponent={<Text>No recent documents found.</Text>}
+                    contentContainerStyle={{justifyContent: 'space-between', alignItems: 'center'}}
+                    horizontal={true}
+                />
+            </View>
+        </ScrollView>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-        padding: 20,
+        paddingVertical: 20,
     },
     title: {
         fontSize: 20,
@@ -100,11 +163,10 @@ const styles = StyleSheet.create({
     },
     rewardsContainer: {
         flexDirection: 'row',
-        backgroundColor: 'indigo',
-        padding: 15,
+        padding: 22,
         paddingBottom: 20,
         borderRadius: 10,
-        height: 210,
+        height: 105,
         alignItems: 'flex-end',
         shadowColor: '#000',
         shadowOffset: {
@@ -114,10 +176,15 @@ const styles = StyleSheet.create({
         shadowOpacity: 0.25,
         shadowRadius: 3.84,
         elevation: 5,
+        overflow: 'hidden',
     },
     recentDocItem: {
         alignItems: "center",
         marginRight: 15,
-        paddingHorizontal: 10
+        paddingHorizontal: 10,
+        backgroundColor: Colors.white,
+        borderRadius: 5,
+        borderWidth: 0.5,
+        padding: 10,
     }
 });
--- components/trips/trip-reports/TripReportCard.tsx
+++ components/trips/trip-reports/TripReportCard.tsx
@@ -1,38 +1,194 @@
-import {Text, StyleSheet, View, Dimensions, Pressable, FlatList} from 'react-native';
+import {Text, StyleSheet, View, Dimensions, Pressable, ImageBackground} from 'react-native';
 import Card from "@/components/ui/Card";
 import {Colors} from "@/utils/Colors";
 import {Image} from "expo-image";
 import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 import React from "react";
-import {showLocation} from "react-native-map-link";
-import Ionicons from "@expo/vector-icons/Ionicons";
-import DotPagination from "@/components/ui/DotPagination";
 import {router} from "expo-router";
 import ShareButton from "@/components/ui/buttons/ShareButton";
 
 const {width} = Dimensions.get('screen');
 
+const quote = {
+    imageUrl: "https://flygreen.s3.us-east-2.amazonaws.com/aircraft-category-pictures/supermidsize-exterior.jpg",
+}
+
+const tripSheet = {
+    id: 1,
+    departureDate: "Jun 14th, 10:00am",
+    tailNumber: "N350HH",
+    pilotInCommand: "William Chau",
+    secondInCommand: "Keith Chau",
+    cabinAttendant: "William Chau",
+    segments: [
+        {
+            from: "CYUL to CYYZ",
+            date: "05/14 10:00am",
+            duration: "2h 30m",
+            passengers: "5 PAX",
+            departureAirport: {
+                "name": "Los Angeles Intl",
+                "code": "KLAX",
+                "city": "Los Angeles",
+                "phone": "+1 310-410-9605"
+            },
+            departureFbo: {
+                "name": "Signature Flight Support",
+                "address": "6201 W Imperial Hwy \nLos Angeles, California 90045",
+                "phone": "+1 310-410-9605"
+            },
+            destinationAirport: {
+                "name": "Charlottetown",
+                "code": "CYYG",
+                "city": "Charlottetown",
+                "phone": "+1 310-410-9605"
+            },
+            destinationFbo: {
+                "name": "Main Terminal",
+                "address": "250 Maple Hills Ave, \nCharlottetown, PE, C1C 1N2",
+                "phone": "+1 902-566-7997"
+            }
+        },
+        {
+            from: "CYUL to CYYZ",
+            date: "05/14 10:00am",
+            duration: "2h 30m",
+            passengers: "5 PAX",
+            departureAirport: {
+                "name": "Los Angeles Intl",
+                "code": "KLAX",
+                "city": "Los Angeles",
+                "phone": "+1 310-410-9605"
+            },
+            departureFbo: {
+                "name": "Signature Flight Support",
+                "address": "6201 W Imperial Hwy \nLos Angeles, California 90045",
+                "phone": "+1 310-410-9605"
+            },
+            destinationAirport: {
+                "name": "Charlottetown",
+                "code": "CYYG",
+                "city": "Charlottetown",
+                "phone": "+1 310-410-9605"
+            },
+            destinationFbo: {
+                "name": "Main Terminal",
+                "address": "250 Maple Hills Ave, \nCharlottetown, PE, C1C 1N2",
+                "phone": "+1 902-566-7997"
+            }
+        },
+        {
+            from: "CYUL to CYYZ",
+            date: "05/14 10:00am",
+            duration: "2h 30m",
+            passengers: "5 PAX",
+            departureAirport: {
+                "name": "Los Angeles Intl",
+                "code": "KLAX",
+                "city": "Los Angeles",
+                "address": "6201 W Imperial Hwy \nLos Angeles, California 90045",
+                "phone": "+1 310-410-9605"
+            },
+            departureFbo: {
+                "name": "Signature Flight Support",
+                "address": "6201 W Imperial Hwy \nLos Angeles, California 90045",
+                "phone": "+1 310-410-9605"
+            },
+            destinationAirport: {
+                "name": "Charlottetown",
+                "code": "CYYG",
+                "city": "Charlottetown",
+                "address": "6201 W Imperial Hwy \nLos Angeles, California 90045",
+                "phone": "+1 310-410-9605"
+            },
+            destinationFbo: {
+                "name": "Main Terminal",
+                "address": "250 Maple Hills Ave, \nCharlottetown, PE, C1C 1N2",
+                "phone": "+1 902-566-7997"
+            }
+        }
+    ],
+    passengers: [
+        "John Doe",
+        "Jane Smith",
+        "Bob Johnson",
+        "Alice Brown",
+        "Charlie White"
+    ],
+    notes: "<p><b>Catering: Stevie's Catering</b></p><p><b>Breakfast:</b> 3x Scrambled Eggs + Bacon // 2x Breakfast Burrito<br><b>Lunch: </b>Crispy Vegetable Spring Rolls // Pico de Gallo + Spicy Guacamole &amp; Chips // 3x Chicken Teriyaki Plates // 4x House Salad with Chicken // 1x Steak // 1x Macaroni and Cheese // 1x Spaghetti Tomato Sauce<br><b>Drinks:</b> Coors Light // 2x bottles of Pinot Noir</p><p>&nbsp;</p><p><b>CYYG Driver:</b> 1 902 439-4636</p>",
+}
+
 export default function TripSheetCard() {
+    const [pagination, setPagination] = React.useState(0);
+
     return (
         <Card style={styles.mainContainer}>
-            <View style={styles.rowContainer}>
-                <View>
-                    <Text style={styles.subTitle}>FLIGHT DISTANCE</Text>
-                    <Text>2588 nm</Text>
+            <ImageBackground
+                source={{uri: 'https://flygreen.s3.us-east-2.amazonaws.com/pdf-assets/rewards/gold.jpg'}}
+                imageStyle={{borderTopRightRadius: 10, borderTopLeftRadius: 10}}
+                resizeMode={'cover'}
+            >
+                <View style={[styles.bottomBar, {paddingBottom: 0}]}>
+                    <View>
+                        <Text style={[styles.crewTitle, {color: Colors.white}]}>Flight Distance</Text>
+                        <Text style={styles.departureDate}>413 nautical miles</Text>
+                    </View>
+                    <View>
+                        <Text style={[styles.crewTitle, {color: Colors.white}, styles.right]}>Hours Flown</Text>
+                        <Text style={styles.tailNumber}>1 Hour 40 Minutes</Text>
+                    </View>
                 </View>
-                <View>
-                    <Text style={styles.subTitle}>TIME FLOWN</Text>
-                    <Text>6 hours 17 minutes</Text>
+                <View style={[styles.bottomBar, {borderTopRightRadius: 0, borderTopLeftRadius: 0}]}>
+                    <View>
+                        <Text style={[styles.crewTitle, {color: Colors.white}]}>Max Altitude</Text>
+                        <Text style={styles.departureDate}>43,000 feet</Text>
+                    </View>
+                    <View>
+                        <Text style={[styles.crewTitle, {color: Colors.white}, styles.right]}>Acquired Points</Text>
+                        <Text style={styles.tailNumber}>12,225</Text>
+                    </View>
                 </View>
+            </ImageBackground>
+            <View>
+                <Image
+                    style={[styles.image]}
+                    source={quote.imageUrl}
+                    contentFit="cover"
+                    transition={1000}
+                />
             </View>
-            <View style={styles.rowContainer}>
-                <View>
-                    <Text style={styles.subTitle}>CRUISING ALTITUDE</Text>
-                    <Text>{(41000).toLocaleString()} feet</Text>
+            <View style={{padding: 15}}>
+                <View style={styles.itineraryContainer}>
+                    <View style={styles.segment}>
+                        <View>
+                            <Text style={styles.tripInfoValue}>695,900</Text>
+                            <Text>Tier Points</Text>
+                        </View>
+                        <View>
+                            <Text>304,100</Text>
+                            <Text>Points To Access Elite Tier</Text>
+                        </View>
+                        <View>
+                            <Text>933,400</Text>
+                            <Text>Redeemable Points</Text>
+                        </View>
+                        <View>
+                            <Text>$9,334</Text>
+                            <Text>Available Cash</Text>
+                        </View>
+                    </View>
                 </View>
-                <View>
-                    <Text style={styles.subTitle}>Points Earned</Text>
-                    <Text>{(15000).toLocaleString()}</Text>
+                <View style={styles.buttonsContainer}>
+                    <Pressable
+                        style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'flex-end', gap: 5}}
+                        onPress={() => {
+                            router.push(`/trip-sheet/${tripSheet.id}`);
+                        }}
+                    >
+                        <Text style={{color: Colors.flygreenGreen}}>View More</Text>
+                        <MaterialCommunityIcons name="file-document-outline" size={20} color={Colors.flygreenGreen} />
+                    </Pressable>
+                    <ShareButton shareUrl="http://www.flygreen.test/trip-sheet/1" dialogTitle="Share Trip Sheet" buttonText="Share" mimeType="text/plain" />
                 </View>
             </View>
         </Card>
@@ -40,19 +196,64 @@ export default function TripSheetCard() {
 }
 
 const styles = StyleSheet.create({
+    buttonsContainer: {
+        marginTop: 15,
+        flexDirection: "row",
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+        gap: 10,
+    },
+    airportInfo: {
+        flex: 2,
+        gap: 5
+    },
+    airportCode: {
+        fontSize: 30,
+        color: "#205046"
+    },
+    airportName: {
+        fontSize: 12,
+        color: 'gray'
+    },
+    segment: {
+        gap: 5
+    },
     mainContainer: {
         marginHorizontal: 10,
+        width: width-20,
+        padding: 0
     },
-    subTitle: {
+    container: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    bottomBar: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        borderTopRightRadius: 10,
+        borderTopLeftRadius: 10,
+        padding: 15
+    },
+    tailNumber: {
+        color: Colors.white,
+        fontWeight: 'bold',
+        fontSize: 16,
+        textAlign: 'right',
+    },
+    departureDate: {
+        color: Colors.white,
+        fontWeight: 'bold',
+        fontSize: 16,
+        textAlign: 'right',
+    },
+    crewTitle: {
         fontSize: 12,
         color: '#888',
     },
-    value: {
+    crewName: {
         fontSize: 16,
-    },
-    rowContainer: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
     },
     middle: {
         textAlign: 'center',
@@ -60,4 +261,28 @@ const styles = StyleSheet.create({
     right: {
         textAlign: 'right',
     },
+    image: {
+        flex: 1,
+        backgroundColor: '#0553',
+        padding: 70,
+        width: "100%",
+        resizeMode: 'contain',
+        zIndex: 1
+    },
+    topRadius: {
+        borderTopRightRadius: 10,
+        borderTopLeftRadius: 10,
+    },
+    itineraryContainer: {
+        marginTop: 5
+    },
+    passengerContainer: {
+        marginTop: 5,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+    },
+    tripInfoValue: {
+
+    }
 });
